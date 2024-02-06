e548f4c0f64bafaa1168c905dc8640f58055d6c0
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-02-06 15:36:39 +0100

    feat: comparsa simultanea di tutte le 10 email

--- js/main.js
+++ js/main.js
@@ -10,15 +10,14 @@ createApp({
 
   methods: {
     generateEmail() {
-      this.emailArray = [];
       for (let i = 0; i < 10; i++) {
-        axios
-          .get("https://flynn.boolean.careers/exercises/api/random/mail")
-          .then((result) => {
-            this.emailArray.push(result.data.response);
-            console.log(result.data.response);
-            console.log(this.emailArray);
-          });
+        (this.emailArray = []),
+          axios
+            .get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((result) => {
+              this.emailArray.push(result.data.response);
+              console.log(result.data.response);
+            });
       }
     },
   },
