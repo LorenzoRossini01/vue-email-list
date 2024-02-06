d15394b8b0fceab54b50d6f598d1e03293b06137
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-02-06 15:54:02 +0100

    feat: aggiunta loading screen

--- js/main.js
+++ js/main.js
@@ -5,11 +5,13 @@ createApp({
     return {
       title: "Vue email list",
       emailArray: [],
+      loading: false,
     };
   },
 
   methods: {
     generateEmail() {
+      this.loading = true;
       for (let i = 0; i < 10; i++) {
         (this.emailArray = []),
           axios
@@ -17,6 +19,9 @@ createApp({
             .then((result) => {
               this.emailArray.push(result.data.response);
               console.log(result.data.response);
+              if (this.emailArray.length == 10) {
+                this.loading = false;
+              }
             });
       }
     },
